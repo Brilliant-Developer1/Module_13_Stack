d63b8d6afd2cb699de096a300ccca2756eaa3f3f
Brilliant-Developer1 <email2obail@gmail.com>
2023-07-01 02:03:40 +0600

    first commit

---
+++ stack_array.cpp
@@ -0,0 +1,133 @@
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+class myStack
+{
+public:
+    vector<int> v;
+    void push(int value)
+    {
+        v.push_back(value);
+    }
+    void pop()
+    {
+        v.pop_back();
+    }
+    int top()
+    {
+        return v.back();
+    }
+    int size()
+    {
+        return v.size();
+    }
+    bool empty()
+    {
+        if (v.size() == 0)
+            return true;
+        else
+            return false;
+    }
+};
+int main()
+{
+    myStack st;
+    int input;
+    cin >> input;
+    for (int i = 0; i < input; i++)
+    {
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+
+    while (!st.empty())
+    {
+        cout << st.top() << endl;
+        st.pop();
+    }
+    // while (st.empty() == true)
+    // {
+    //     cout << st.top() << endl;
+    //     st.pop();
+    // }
+
+    // st.push(10);
+    // st.push(20);
+    // cout << st.top() << endl;
+    // st.pop();
+    // cout << st.top() << endl;
+    // st.pop();
+    // // st.push(30);
+
+    // if (st.empty() == true)
+    // {
+    //     cout << st.top() << endl;
+    // }
+    // if (st.empty() == false)
+    // {
+
+    //     cout << "Stack is empty";
+    // }
+
+    return 0;
+}
