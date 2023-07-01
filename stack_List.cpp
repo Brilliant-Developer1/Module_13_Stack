5fff0e7cb33a8d9ed138043e25efad10a05d7ed2
Brilliant-Developer1 <email2obail@gmail.com>
2023-07-01 21:46:24 +0600

    final

--- stack_List.cpp
+++ stack_List.cpp
@@ -60,9 +60,37 @@
 
 using namespace std;
 
+class myStack
+{
+public:
+    list<int> v;
+    void push(int value)
+    {
+        v.push_back(value);
+    }
+    void pop()
+    {
+        v.pop_back();
+    }
+    int top()
+    {
+        return v.back();
+    }
+    int size()
+    {
+        return v.size();
+    }
+    bool empty()
+    {
+        if (v.size() == 0)
+            return true;
+        else
+            return false;
+    }
+};
 int main()
 {
-    stack<int> st;
+    myStack st;
     int input;
     cin >> input;
     for (int i = 0; i < input; i++)
